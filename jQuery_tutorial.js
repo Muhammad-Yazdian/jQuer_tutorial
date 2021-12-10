f404e17bd819d2d2e37ef20d0477b2ca062dc354
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-12-10 12:44:26 +0100

    Added header comment

--- jQuery_tutorial.js
+++ jQuery_tutorial.js
@@ -1,3 +1,9 @@
+/**
+ * jQury Tutorial
+ * This file is not linked to any HTML file.
+ * It just lists some of most common jQuery commands
+ */
+
 // To execute a function when DOM is fully loaded
 $(document).ready(function(){});
 
