bc67e52518c981637a9d7acd054ed9b49c78bea5
Muhammad-Yazdian <93126501+Muhammad-Yazdian@users.noreply.github.com>
2021-12-10 10:02:04 +0100

    Create jQuery_tutorial.js

---
+++ jQuery_tutorial.js
@@ -0,0 +1,6 @@
+// To execute a function when DOM is fully loaded
+$(document).ready(function(){});
+
+// Load data form server and place HTML
+$(window).load(function(){});
+$('#box').load(function(){});
